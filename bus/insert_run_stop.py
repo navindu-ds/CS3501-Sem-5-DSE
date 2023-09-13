0de95a678b3932cb8115243aedf7cf4835d201a1
Tharusha Bandaranayake <114094530+TheNobleStag@users.noreply.github.com>
2023-09-13 14:57:26 +0530

    Delete bus directory

--- bus/insert_run_stop.py
+++
@@ -1,7 +0,0 @@
-from insert_runtime import addRunningTime
-from insert_stop_time import addStopTime
-
-
-def addData(startTime, endTime):
-    addStopTime(startTime, endTime)
-    addRunningTime(startTime, endTime)
